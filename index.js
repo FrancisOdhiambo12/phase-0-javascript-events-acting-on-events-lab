781a3fabf6cbe1144e5a6113f5c38fe528423f60
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-06-19 12:34:26 +0300

    Moving dodger to the right

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+  
+    if ( left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+        
+  document.addEventListener('keydown', function(e) {
+    if ( e.key === 20 ) {
+      moveDodgerLeft();
+    }
+    if ( e.key === 22 ) {
+      moveDodgerRight();
+    }
+  });
+
+
+// function moveDodgerRight() {
+//     const rightNumbers = dodger.style.right.replace("px", "")
+//     const left = parseInt(rightNumbers, 10)
+
+//     if (left < 0) {
+//         dodger.style.left = `${left + 1}px`
+//     }
+// }
+//     document.addEventListener("keydown", function (e) {
+//         if (e.key === "ArrowRight") {
+//             moveDodgerRight
+//         }
+//     })
+
+// moving dodger back to right position
